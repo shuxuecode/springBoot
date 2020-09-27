0b2cfcdbcc74105842a92e6009f0e364c2c2c7aa
zhaoshuxue <zhaoshuxue@163.com>
2020-09-27 21:25:41 +0800

    commitByZsx

--- code/code-demo/src/test/java/hashMapTest/Test1.java
+++ code/code-demo/src/test/java/hashMapTest/Test1.java
@@ -4,11 +4,28 @@ import org.junit.jupiter.api.Test;
 
 public class Test1 {
 
+
+    @Test
+    public void test3() {
+
+        int n = 8;
+        for (int i = 0; i < 10; i++) {
+            System.out.print(n);
+            System.out.print(" ===  ");
+            System.out.println(Integer.toBinaryString(n));
+            n |= n >>> 1;
+        }
+
+    }
+
     @Test
     public void test2() {
 
-        int i = 4;
-        System.out.println(Integer.toBinaryString(i));
+        for (int i = 1; i < 20; i++) {
+            System.out.print(i);
+            System.out.print(" --  ");
+            System.out.println(Integer.toBinaryString(i));
+        }
 
     }
 
