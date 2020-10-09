e9847db518bd9234009a3d8f481e70430d9cf1c1
zhaoshuxue <zhaoshuxue@163.com>
2020-10-09 19:12:18 +0800

    commitByZsx

---
+++ springboot2.2/springboot-flink/src/main/java/com/zsx/springbootflink/SpringbootFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.zsx.springbootflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFlinkApplication.class, args);
+    }
+
+}
---
+++ springboot2.2/springboot-flink/src/test/java/com/zsx/springbootflink/SpringbootFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsx.springbootflink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
