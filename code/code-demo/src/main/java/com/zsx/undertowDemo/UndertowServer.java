eb312c5220328605d06db013a4faf8cda1dfee5d
zhaoshuxue <zhaoshuxue@163.com>
2020-12-16 20:18:55 +0800

    commitByZsx

---
+++ code/code-demo/src/main/java/com/zsx/undertowDemo/UndertowServer.java
@@ -0,0 +1,25 @@
+package com.zsx.undertowDemo;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class UndertowServer {
+
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+                    @Override
+                    public void handleRequest(HttpServerExchange exchange) throws Exception {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello world!!!");
+                    }
+                }).build();
+
+        server.start();
+
+    }
+
+}
