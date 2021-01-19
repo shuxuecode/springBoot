c6c677711c05f57b80988343e9bcdec39d8fd4af
zhaoshuxue <zhaoshuxue@163.com>
2021-01-19 21:38:01 +0800

    commitByZsx

---
+++ code/code-demo/src/test/java/javaCore/reflectTest2/Test2.java
@@ -0,0 +1,16 @@
+package javaCore.reflectTest2;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class Test2 {
+
+
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+
+
+
+
+    }
+
+}
