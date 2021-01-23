f33aa97c50e7f301efdc46f3fa1e90ec363d0d1c
zhaoshuxue <zhaoshuxue@163.com>
2021-01-23 21:39:55 +0800

    commitByZsx

--- springboot2.2/springboot-kisso/src/test/java/JavaTest.java
+++ springboot2.2/springboot-kisso/src/test/java/JavaTest.java
@@ -16,13 +16,11 @@ public class JavaTest {
 
     @Test
     public void test2(){
-//        if(((double)Math.abs((user.hashCode())%1000)/1000)>identitySdkMarkRatio){
 
-
-        double v = ((double) Math.abs("树学网店".hashCode()) % 1000) / 1000;
+        double v = ((double) Math.abs("".hashCode()) % 1000) / 1000;
         System.out.println(v);
 
-        v = ((double) Math.abs("猫太郎么么".hashCode()) % 1000) / 1000;
+        v = ((double) Math.abs("".hashCode()) % 1000) / 1000;
         System.out.println(v);
     }
 
