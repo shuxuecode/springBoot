1735c021d3e1ca7399e675ae63b8fc38edf70f74
zhaoshuxue <zhaoshuxue@163.com>
2021-03-09 23:54:50 +0800

    commitByZsx

--- code/code-demo/src/test/java/excel/easyexcelTest.java
+++ code/code-demo/src/test/java/excel/easyexcelTest.java
@@ -1,11 +1,16 @@
 package excel;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.analysis.ExcelReadExecutor;
 import com.alibaba.excel.read.builder.ExcelReaderBuilder;
+import com.alibaba.excel.read.metadata.ReadSheet;
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.alibaba.excel.write.ExcelBuilder;
 import com.alibaba.excel.write.metadata.WriteSheet;
+
+import java.util.List;
 
 public class easyexcelTest {
 
@@ -28,5 +33,18 @@ public class easyexcelTest {
 
         excelWriter.finish();
 
+
+        ExcelReader excelReader = EasyExcel.read("test.xlsx").excelType(ExcelTypeEnum.XLSX).build();
+
+        ExcelReadExecutor excelReadExecutor = excelReader.excelExecutor();
+
+        List<ReadSheet> sheetList = excelReadExecutor.sheetList();
+
+        for (ReadSheet readSheet : sheetList) {
+            String sheetName = readSheet.getSheetName();
+            System.out.println(sheetName);
+        }
+
+        excelReader.finish();
     }
 }
