e7d169793020cf83763eac9169b47105ea2b85ae
zhaoshuxue <zhaoshuxue@163.com>
2021-03-09 23:44:18 +0800

    commitByZsx

--- code/code-demo/src/test/java/excel/easyexcelTest.java
+++ code/code-demo/src/test/java/excel/easyexcelTest.java
@@ -3,7 +3,9 @@ package excel;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.read.builder.ExcelReaderBuilder;
+import com.alibaba.excel.support.ExcelTypeEnum;
 import com.alibaba.excel.write.ExcelBuilder;
+import com.alibaba.excel.write.metadata.WriteSheet;
 
 public class easyexcelTest {
 
@@ -11,9 +13,20 @@ public class easyexcelTest {
 
 
 //        EasyExcel
-        ExcelReaderBuilder readerBuilder = EasyExcel.read();
+//        ExcelReaderBuilder readerBuilder = EasyExcel.read();
 
-        
+
+//        EasyExcel.read("");
+
+
+        ExcelWriter excelWriter = EasyExcel.write("test.xlsx").excelType(ExcelTypeEnum.XLSX).build();
+
+        WriteSheet sheet = new WriteSheet();
+        sheet.setSheetName("demo");
+
+        excelWriter.write(null, sheet);
+
+        excelWriter.finish();
 
     }
 }
