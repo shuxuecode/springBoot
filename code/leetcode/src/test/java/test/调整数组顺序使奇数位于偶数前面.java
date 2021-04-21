93dea50c07087ff57675505fbbd2a850dca35067
zhaoshuxue <zhaoshuxue@163.com>
2021-04-22 01:05:14 +0800

    commitByZsx

--- code/leetcode/src/test/java/test/调整数组顺序使奇数位于偶数前面.java
+++ code/leetcode/src/test/java/test/调整数组顺序使奇数位于偶数前面.java
@@ -19,10 +19,12 @@ public class 调整数组顺序使奇数位于偶数前面 {
     public void test() {
         int[] arr = {1, 2, 3, 4, 5, 6, 12, 7, 8, 9, 10};
 //        int[] arr = {1, 2, 3, 4};
-        reOrderArray(arr);
+//        reOrderArray(arr);
+        reOrderArray2(arr);
 
         for (int i : arr) {
-            System.out.println(i);
+            System.out.print(i);
+            System.out.print(" , ");
         }
     }
 
@@ -32,15 +34,35 @@ public class 调整数组顺序使奇数位于偶数前面 {
         int end = array.length - 1;
 
         while (start < end) {
-            while (array[start] % 2 == 1) {
+            while (start < end && array[start] % 2 == 1) {
+                // 奇数跳过
                 start++;
             }
-            swap1(array, start, end);
-
-            while (array[end] % 2 == 0) {
-                end--;
+            if (start >= end) {
+                break;
             }
-            swap2(array, start, end);
+            // start 表示当前偶数的坐标，需要寻找下一个奇数的坐标
+            int tmp = start + 1;
+            while (tmp < end && array[tmp] % 2 == 0) {
+                // 偶数跳过
+                tmp++;
+            }
+            if (tmp >= end) {
+                break;
+            }
+            // tmp 表示奇数的坐标
+            // 此时需要，从tmp坐标依次跟前一个元素进行交换，最后到start坐标为止
+
+            for (int i = tmp; i > start; i--) {
+                swap(array, i - 1, i);
+            }
+            //
+//            start = tmp;
+
+//            while (array[end] % 2 == 0) {
+//                end--;
+//            }
+//            swap2(array, start, end);
         }
 
         System.out.println("start " + start);
@@ -48,37 +70,65 @@ public class 调整数组顺序使奇数位于偶数前面 {
 
     }
 
-    public void swap1(int[] array, int start, int end) {
-        int i = start;
-        i++;
-        // 寻找下一个奇数
-        while (array[i] % 2 == 1) {
-            i++;
-        }
-        if (i < end) {
-            while (i > start) {
-                int temp = array[i];
-                array[i] = array[i - 1];
-                array[i - 1] = temp;
-                i--;
+    public void reOrderArray2(int[] array) {
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start < end) {
+            while (start < end && array[start] % 2 == 1) {
+                // 奇数跳过
+                start++;
+            }
+            if (start >= end) {
+                break;
+            }
+            // start 表示当前偶数的坐标，需要寻找下一个奇数的坐标
+            int m = start + 1;
+            while (m < end && array[m] % 2 == 0) {
+                // 偶数跳过
+                m++;
+            }
+            // m 表示奇数的坐标
+            // 此时需要，从 m 坐标依次跟前一个元素进行交换，最后到start坐标为止
+            for (int i = m; i > start; i--) {
+                swap(array, i - 1, i);
+            }
+
+//            while (array[end] % 2 == 0) {
+//                end--;
+//            }
+//            swap2(array, start, end);
+            while (start < end && array[end] % 2 == 0) {
+                // 偶数跳过
+                end--;
+            }
+            if (start >= end) {
+                break;
+            }
+            // end 表示当前奇数的坐标，需要寻找下一个偶数的坐标
+            int n = end - 1;
+            while (n > end && array[n] % 2 == 1) {
+                // 奇数跳过
+                n--;
+            }
+            if (n <= start) {
+                break;
+            }
+            // n 表示偶数的坐标
+            // 此时需要，从 n 坐标依次跟后一个元素进行交换，最后到 end 坐标为止
+            for (int i = n; i < end; i++) {
+                swap(array, i, i + 1);
             }
         }
+
+        System.out.println("start " + start);
+        System.out.println("end " + end);
     }
 
-    public void swap2(int[] array, int start, int end) {
-        int i = end;
-        i--;
-        // 寻找下一个偶数
-        while (array[i] % 2 == 0) {
-            i--;
-        }
-        if (start < i) {
-            while (i < end) {
-                int temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                i++;
-            }
-        }
+    public void swap(int[] array, int a, int b) {
+        int temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
