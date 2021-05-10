4f772d3bb757ab3ddcaed91fbf3651ca3cf2d986
zhaoshuxue <zhaoshuxue@163.com>
2021-05-11 00:27:21 +0800

    commitByZsx

---
+++ code/leetcode/src/test/java/leetcode/N4.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import org.junit.jupiter.api.Test;
+
+public class N4 {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        return 0;
+    }
+
+
+    @Test
+    void test() {
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3, 4, 5};
+
+        double num = findMedianSortedArrays(nums1, nums2);
+
+        System.out.println(num);
+
+    }
+}
