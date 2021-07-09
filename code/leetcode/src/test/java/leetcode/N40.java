a36e8c8afa060f72413b9e3ca8900c4c9345c2d6
zhaoshuxue <zhaoshuxue@163.com>
2021-07-09 16:20:49 +0800

    leetcode

--- code/leetcode/src/test/java/leetcode/N40.java
+++ code/leetcode/src/test/java/leetcode/N40.java
@@ -2,43 +2,63 @@ package leetcode;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * todo
  */
 public class N40 {
 
-    List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
 
-        for (int i = 0; i < candidates.length; i++) {
-            dfs(candidates, i, target, 0, new ArrayList<>());
+        int length = candidates.length;
+        if (length == 0) {
+            return result;
         }
+
+        // 排序
+        Arrays.sort(candidates);
+
+        // 存储已存在的路径
+        Deque<Integer> path = new ArrayDeque<>(length);
+
+        dfs(candidates, length, 0, target, path, result);
 
         return result;
     }
 
-    public void dfs(int[] candidates, int i, int target, int sum, List<Integer> list) {
-        if (i == candidates.length) {
+    /**
+     * @param candidates
+     * @param len
+     * @param begin      从候选数组的 begin 位置开始搜索
+     * @param target     表示剩余，这个值一开始等于 target，基于题目中说明的"所有数字（包括目标数）都是正整数"这个条件
+     * @param path       从根结点到叶子结点的路径
+     * @param res
+     */
+    public void dfs(int[] candidates, int len, int begin, int target, Deque<Integer> path, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
             return;
         }
-        sum += candidates[i];
-        list.add(candidates[i]);
 
-        if (sum > target) {
-            return;
-        }
-        if (sum == target) {
-            result.add(list);
-            return;
-        }
+        for (int i = begin; i < len; i++) {
+            //
+            if (target - candidates[i] < 0) {
+                break;
+            }
 
-        dfs(candidates, i + 1, target, sum, list);
+            if (i > begin && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            path.addLast(candidates[i]);
+
+            dfs(candidates, len, i + 1, target - candidates[i], path, res);
+
+            path.removeLast();
+        }
     }
 
 
