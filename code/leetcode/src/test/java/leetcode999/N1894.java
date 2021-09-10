4eff72ee62caeb746381e900eeca5a126c77ad29
shuxuecode <zhaoshuxue@163.com>
2021-09-11 00:06:27 +0800

    commitByZsx

--- code/leetcode/src/test/java/leetcode999/N1894.java
+++ code/leetcode/src/test/java/leetcode999/N1894.java
@@ -2,16 +2,13 @@ package leetcode999;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 /**
  * @author zsx
  * @date 2021/9/10
  */
 public class N1894 {
-    public int chalkReplacer(int[] chalk, int k) {
-        int res = 0;
-
+    // 错误
+    public int chalkReplacer1(int[] chalk, int k) {
         int n = chalk.length;
 
         int[] arr = new int[n];
@@ -36,11 +33,34 @@ public class N1894 {
         return 0;
     }
 
+    public int chalkReplacer(int[] chalk, int k) {
+
+
+        int n = chalk.length;
+
+        long count = chalk[0];
+
+        for (int i = 1; i < n; i++) {
+            count += chalk[i];
+        }
+
+        long j = k % count;
+
+        for (int i = 0; i < n; i++) {
+            j -= chalk[i];
+            if (j < 0) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
 
     @Test
     void t() {
-        int i = chalkReplacer(new int[]{5, 1, 5}, 22);
-        //int i = chalkReplacer(new int[]{3, 4, 1, 2}, 25);
+        //int i = chalkReplacer(new int[]{5, 1, 5}, 22);
+        int i = chalkReplacer(new int[]{3, 4, 1, 2}, 25);
 
         System.out.println(i);
     }
