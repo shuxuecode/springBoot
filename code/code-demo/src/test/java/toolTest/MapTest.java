ac948b402c466866031014b7fa7241d77def7f2b
zhaoshuxue <zhaoshuxue@163.com>
2021-11-07 20:16:04 +0800

    commitByZsx

--- code/code-demo/src/test/java/toolTest/MapTest.java
+++ code/code-demo/src/test/java/toolTest/MapTest.java
@@ -15,7 +15,7 @@ public class MapTest {
 
     private static Striped<Lock> striped = Striped.lazyWeakLock(10);
 
-    private static Lock lock = new ReentrantLock();
+    private static volatile Lock lock = new ReentrantLock();
 
     private static volatile String flag = "a";
 
@@ -65,6 +65,38 @@ public class MapTest {
 //            lock.unlock();
                 System.out.println("unlock" + num);
             }
+            System.out.println(num + " : " + (System.currentTimeMillis() - start));
+        }
+    }
+
+
+    @Test
+    void t1() throws InterruptedException {
+        start = System.currentTimeMillis();
+        System.out.println("开始： " + start);
+
+        for (int i = 0; i < 5; i++) {
+            int num = i;
+            CompletableFuture.runAsync(() -> {
+                new MapTest().run1(num + 100);
+            });
+        }
+
+        TimeUnit.SECONDS.sleep(15L);
+    }
+
+    private void run1(int num) {
+        System.out.println("开始执行" + num);
+        try {
+            lock.lock();
+            System.out.println("lock" + num);
+            System.out.println("执行逻辑 " + num);
+            TimeUnit.SECONDS.sleep(1L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+            System.out.println("unlock" + num);
             System.out.println(num + " : " + (System.currentTimeMillis() - start));
         }
     }
