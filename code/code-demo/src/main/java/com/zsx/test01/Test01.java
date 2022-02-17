5000e2a67d2480d32013861be4e76b7aebed243d
shuxuecode <zhaoshuxue@163.com>
2022-02-17 21:19:21 +0800

    commitByZsx

---
+++ code/code-demo/src/main/java/com/zsx/test01/Test01.java
@@ -0,0 +1,14 @@
+package com.zsx.test01;
+
+
+/**
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+
+
+        String str = "[{\"amount\":1,\"templateId\":\"1\"}]";
+
+    }
+}
