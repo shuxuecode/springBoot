b82062a18a09cb22da5c4a09e2df454d5613d877
shuxuecode <zhaoshuxue@163.com>
2022-03-02 23:10:37 +0800

    commitByZsx

--- code/code-demo/src/main/java/com/zsx/test01/Test01.java
+++ code/code-demo/src/main/java/com/zsx/test01/Test01.java
@@ -1,6 +1,12 @@
 package com.zsx.test01;
 
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  */
 public class Test01 {
@@ -10,5 +16,9 @@ public class Test01 {
 
         String str = "[{\"amount\":1,\"templateId\":\"1\"}]";
 
+        ArrayList<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        List<Integer> collect = list.stream().filter(item -> item.equals(0)).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
