cdd4a635a4821fcc4cad4202e632c8a6eeffa652
zhaoshuxue <zhaoshuxue@163.com>
2022-04-05 23:30:54 +0800

    commitByZsx

---
+++ springboot2.6/springboot-bean-init-async/src/main/java/com/example/springboot/bean/UserServiceFactoryBean.java
@@ -0,0 +1,19 @@
+package com.example.springboot.bean;
+
+import com.example.springboot.service.UserService;
+import com.example.springboot.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServiceFactoryBean implements FactoryBean {
+    @Override
+    public Object getObject() throws Exception {
+        return new UserServiceImpl();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return UserService.class;
+    }
+}
