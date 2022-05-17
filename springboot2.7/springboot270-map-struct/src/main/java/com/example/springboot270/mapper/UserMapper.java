59dd67d8c3d764ed9fb3509c7ba8710023aed572
shuxuecode <zhaoshuxue@163.com>
2022-05-17 23:09:02 +0800

    commitByZsx

--- springboot2.7/springboot270-map-struct/src/main/java/com/example/springboot270/mapper/UserMapper.java
+++ springboot2.7/springboot270-map-struct/src/main/java/com/example/springboot270/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.example.springboot270.dto.UserDto;
 import com.example.springboot270.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 /**
  * @date 2022/5/17
@@ -11,8 +12,12 @@ import org.mapstruct.Mapping;
 @Mapper
 public interface UserMapper {
 
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
     @Mapping(source = "id", target = "id")
+    //@Mapping(source = "id", ignore = true) // 忽略，不进行映射
     @Mapping(source = "name", target = "userName")
     UserDto userToUserDto(User user);
+
 
 }
