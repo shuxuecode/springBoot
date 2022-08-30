19e2e5b487148ed1f8d5c4abe2935c5a0a3ee908
shuxuecode <zhaoshuxue@163.com>
2022-08-30 21:31:48 +0800

    :hammer: :children_crossing: :iphone:

---
+++ springboot2.5/springboot-mybatis-plus/src/main/java/com/zsx/springbootmybatisplus/interceptor/MybatisInterceptor.java
@@ -0,0 +1,27 @@
+package com.zsx.springbootmybatisplus.interceptor;
+
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Invocation;
+
+import java.util.Properties;
+
+/**
+ * todo
+ * @date 2022/8/30
+ */
+public class MybatisInterceptor implements Interceptor {
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Interceptor.super.plugin(target);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        Interceptor.super.setProperties(properties);
+    }
+}
