fa3513ab16ef64cf8d2fda0de6c2ce38925cb8e6
shuxuecode <zhaoshuxue@163.com>
2022-08-31 20:40:45 +0800

    :poop: :twisted_rightwards_arrows: :camera_flash:

--- springboot2.5/springboot-mybatis-plus/src/main/java/com/zsx/springbootmybatisplus/interceptor/MybatisInterceptor.java
+++ springboot2.5/springboot-mybatis-plus/src/main/java/com/zsx/springbootmybatisplus/interceptor/MybatisInterceptor.java
@@ -1,27 +1,56 @@
 package com.zsx.springbootmybatisplus.interceptor;
 
+import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
+import java.sql.Statement;
 import java.util.Properties;
 
 /**
- * todo
+ * 拦截器
+ * <p>
+ * 1、实现 org.apache.ibatis.plugin.Interceptor 接口
+ * 2、在自定义的拦截器上添加需要拦截的对象和方法，通过注解 org.apache.ibatis.plugin.Intercepts 添加
+ *
  * @date 2022/8/30
  */
+@Component
+@Order(1)
+@Intercepts({
+        @Signature(
+                type = StatementHandler.class,
+                method = "update",
+                args = {Statement.class}
+        )
+})
 public class MybatisInterceptor implements Interceptor {
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        return null;
+
+        invocation.getArgs();
+        invocation.getMethod();
+        invocation.getTarget();
+
+        Object result = invocation.proceed();
+
+        result = result;
+
+        return result;
     }
 
     @Override
     public Object plugin(Object target) {
-        return Interceptor.super.plugin(target);
+        return Plugin.wrap(target, this);
     }
 
     @Override
     public void setProperties(Properties properties) {
-        Interceptor.super.setProperties(properties);
+        System.out.println(properties);
     }
 }
