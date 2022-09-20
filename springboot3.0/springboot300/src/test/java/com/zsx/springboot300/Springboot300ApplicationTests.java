6899c32afdbe7ce931581936b5960a7c0a1fc5bf
shuxuecode <zhaoshuxue@163.com>
2022-09-20 21:37:10 +0800

    :see_no_evil: :adhesive_bandage: :arrow_up:

---
+++ springboot3.0/springboot300/src/test/java/com/zsx/springboot300/Springboot300ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsx.springboot300;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot300ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
