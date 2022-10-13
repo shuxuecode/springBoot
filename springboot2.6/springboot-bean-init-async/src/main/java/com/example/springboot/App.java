d4a1d346938af1bcf7d50db4a2634c4677c246fd
shuxuecode <zhaoshuxue@163.com>
2022-10-13 21:30:58 +0800

    :recycle: :page_facing_up: :art:

--- springboot2.6/springboot-bean-init-async/src/main/java/com/example/springboot/App.java
+++ springboot2.6/springboot-bean-init-async/src/main/java/com/example/springboot/App.java
@@ -2,10 +2,15 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {
+				GroovyTemplateAutoConfiguration.class // 只要在pom里引入groovy的jar包就会加载GroovyMarkupConfigurer，通过排包方式去除
+		}
+)
 @ImportResource(locations = {"classpath:spring/spring-bean.xml"})
 public class App {
 
