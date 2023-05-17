8a2e9e0671f9ec83a393134c2f4fecae1dd33df2
shuxuecode <zhaoshuxue@163.com>
2023-05-17 20:08:50 +0800

    :package: :rotating_light: :bento:

--- springboot2.7/springboot2711-my-excel/src/test/java/com/zsx/springboot2711/Test.java
+++ springboot2.7/springboot2711-my-excel/src/test/java/com/zsx/springboot2711/Test.java
@@ -1,7 +1,15 @@
 package com.zsx.springboot2711;
 
+import com.github.liaochong.myexcel.core.DefaultExcelBuilder;
 import com.github.liaochong.myexcel.core.DefaultStreamExcelBuilder;
+import com.github.liaochong.myexcel.utils.FileExportUtil;
+import org.apache.poi.ss.usermodel.Workbook;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 
@@ -12,13 +20,25 @@ public class Test {
 
 
     @org.junit.jupiter.api.Test
-    void t1(){
-        DefaultStreamExcelBuilder<Map> builder = DefaultStreamExcelBuilder.of(Map.class)
-                .threadPool(Executors.newFixedThreadPool(10))
-                .capacity(10_000)
-                .start();
+    void t1() throws IOException {
+//        DefaultStreamExcelBuilder<Map> builder = DefaultStreamExcelBuilder.of(Map.class)
+//                .threadPool(Executors.newFixedThreadPool(10))
+//                .capacity(10_000)
+//                .start();
 
-//        builder.
+        List<Map> list = new ArrayList<>();
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a", "1");
+        map.put("b", "2");
+        map.put("c", "3");
+
+        list.add(map);
+
+        Workbook workbook = DefaultExcelBuilder.of(Map.class)
+                .build(list);
+
+        FileExportUtil.export(workbook, new File("./test.xlsx"));
 
     }
 
