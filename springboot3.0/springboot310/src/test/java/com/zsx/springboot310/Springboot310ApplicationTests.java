506a702f740acfa18083d86d327d89eefb935ae3
shuxuecode <zhaoshuxue@163.com>
2023-05-24 20:38:21 +0800

    :construction: :card_file_box: :bulb:

---
+++ springboot3.0/springboot310/src/test/java/com/zsx/springboot310/Springboot310ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsx.springboot310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
