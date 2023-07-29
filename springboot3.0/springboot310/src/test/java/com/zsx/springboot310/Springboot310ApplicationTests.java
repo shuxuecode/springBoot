e433b35380c9b17ae434331c1d00af9781ea619b
shuxuecode <zhaoshuxue@163.com>
2023-07-29 14:36:08 +0800

    :poop: :pencil2: :mute:

--- springboot3.0/springboot310/src/test/java/com/zsx/springboot310/Springboot310ApplicationTests.java
+++ springboot3.0/springboot310/src/test/java/com/zsx/springboot310/Springboot310ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.zsx.springboot310;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,10 @@ class Springboot310ApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		String res = StringUtils.joinWith(",", "a", "b");
+		System.out.println(res);
+
 	}
 
 }
