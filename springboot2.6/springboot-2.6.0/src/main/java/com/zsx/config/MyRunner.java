c431db009ac016ba397fb2ef5b61f9b3b0e7ff88
zhaoshuxue <zhaoshuxue@163.com>
2023-10-25 22:25:36 +0800

    :bento: :chart_with_upwards_trend: :lipstick:

--- springboot2.6/springboot-2.6.0/src/main/java/com/zsx/config/MyRunner.java
+++ springboot2.6/springboot-2.6.0/src/main/java/com/zsx/config/MyRunner.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @date 2021/11/24
@@ -18,7 +19,13 @@ public class MyRunner implements CommandLineRunner {
         ApplicationContext applicationContext = SpringBeanUtils.getApplicationContext();
         String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
         System.out.println("打印Spring容器中的bean start");
-        Arrays.stream(beanDefinitionNames).forEach(System.out::println);
+        //Arrays.stream(beanDefinitionNames).forEach(System.out::println);
+        AtomicInteger i = new AtomicInteger();
+        Arrays.stream(beanDefinitionNames).forEach(item -> {
+            System.out.print((i.getAndIncrement()) + " : ");
+            System.out.println(item);
+        });
+
         System.out.println("打印Spring容器中的bean end");
     }
 }
