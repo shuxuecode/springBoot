5a24a70999d03ee4585b1de2424a2aba68a6e994
zhaoshuxue <zhaoshuxue@163.com>
2023-11-24 22:00:58 +0800

    :wrench: :money_with_wings: :tada:

---
+++ springboot3.0/springboot320/src/main/java/com/zsx/springboot320/Springboot320Application.java
@@ -0,0 +1,13 @@
+package com.zsx.springboot320;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot320Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot320Application.class, args);
+	}
+
+}
---
+++ springboot3.0/springboot320/src/test/java/com/zsx/springboot320/Springboot320ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsx.springboot320;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot320ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
