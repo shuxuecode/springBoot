66a1a83ecae37dac59750bb0965328c5e100e776
shuxuecode <zhaoshuxue@163.com>
2025-04-10 21:17:21 +0800

    :speech_balloon: :wastebasket: :see_no_evil:

--- springboot3.0/springboot335/src/main/java/com/springboot335/springboot335/demo/DemoService.java
+++ springboot3.0/springboot335/src/main/java/com/springboot335/springboot335/demo/DemoService.java
@@ -1,9 +1,4 @@
 package com.springboot335.springboot335.demo;
 
-import com.springboot335.springboot335.demo.anno.AAA;
-import com.springboot335.springboot335.demo.anno.BBB;
-
-@AAA
-@BBB
 public class DemoService {
 }
