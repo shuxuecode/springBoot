81d047c73bae42c296669c4bec1a1e0ffb8add15
shuxuecode <zhaoshuxue@163.com>
2025-05-09 18:10:54 +0800

    :green_heart: :twisted_rightwards_arrows: :pencil2:

---
+++ springboot2.1/springboot-redis-lock/src/test/java/BBB.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class BBB {
+
+
+
+
+    @Test
+    public void test01() {
+        // 配置连接池
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxTotal(128); // 最大连接数
+        poolConfig.setMaxIdle(32);  // 最大空闲连接数
+        poolConfig.setMinIdle(8);   // 最小空闲连接数
+
+        // 创建连接池
+        JedisPool jedisPool = new JedisPool(poolConfig, "localhost", 6379);
+
+        try (Jedis jedis = jedisPool.getResource()) {
+            // 执行操作
+            jedis.set("poolKey", "poolValue");
+            System.out.println(jedis.get("poolKey")); // 输出: poolValue
+        }
+
+        // 关闭连接池
+        jedisPool.close();
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+
+        // 创建连接
+        Jedis jedis = new Jedis("localhost", 6379); // 默认端口6379
+
+        // 认证（如果设置了密码）
+        // jedis.auth("password");
+
+        // 测试连接
+        System.out.println("连接成功");
+        System.out.println("服务正在运行: " + jedis.ping());
+
+        // 关闭连接
+        jedis.close();
+
+
+    }
+
+
+}
