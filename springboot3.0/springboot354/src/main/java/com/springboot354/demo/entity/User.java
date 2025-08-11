fbdece4ddffab5adda58da137261e750703efcd7
shuxuecode <zhaoshuxue@163.com>
2025-08-11 21:18:59 +0800

    :zap: :card_file_box: :package:

--- springboot3.0/springboot354/src/main/java/com/springboot354/demo/entity/User.java
+++ springboot3.0/springboot354/src/main/java/com/springboot354/demo/entity/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 import java.util.Date;
 
-@Data
+//@Data
 @Entity
 @Table(name = "t_user")
 public class User {
@@ -23,4 +23,39 @@ public class User {
 
     @Column(nullable = false, name = "created_at")
     private Date createdTime;
+
+
+
+    // getter setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
 }
