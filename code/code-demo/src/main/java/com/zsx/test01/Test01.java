6276361003db3abc01d2009c8969aaf4d4ef1ca9
shuxuecode <zhaoshuxue@163.com>
2025-08-21 22:25:06 +0800

    :speech_balloon: :seedling: :camera_flash:

--- code/code-demo/src/main/java/com/zsx/test01/Test01.java
+++ code/code-demo/src/main/java/com/zsx/test01/Test01.java
@@ -14,8 +14,6 @@ public class Test01 {
     public static void main(String[] args) {
 
 
-        String str = "[{\"amount\":1,\"templateId\":\"1\"}]";
-
         ArrayList<Integer> list = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8);
 
         List<Integer> collect = list.stream().filter(item -> item.equals(0)).collect(Collectors.toList());
